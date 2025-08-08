0c961fe0e233cc05371a46144d25b2e3efb375c7
HaheeBahee <parkadg@naver.com>
2025-08-08 14:14:35 +0900

    blue완료

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class App {
+    public int plus(int num1, int num2){
+        return 3;
+
+    }
+}
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,16 +4,17 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class AppTest {
 
     @Test
     @DisplayName("1과 2를 더하면 3이 나온다.")
     void t1() {
-
         App app = new App();
         int rst = app.plus(1,2);
         //rst가 3인지 자동으로 확인. 검증
-        assertThat(rst).isEqualTo(3);
-    }
+        assertThat(rst).isEqualTo(3);    }
 
 }
